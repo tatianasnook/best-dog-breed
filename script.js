2bb437bf3bd3be5baee367d187784b6a69f7fcce
tatianasnook <tatsnook@gmail.com>
2024-02-17 20:51:02 -0600

    added style and script files

---
+++ script.js
@@ -0,0 +1,47 @@
+const url = `https://api.thedogapi.com/v1/breeds`;
+const api_key = "live_moRqp4X0CNuHIonqcqEd2Ywo6tq5eom2H5KVFu4WSyfifQiOOM1h5kZFRCxdEZ8p"
+let storedBreeds = []
+
+ fetch(url,{headers: {
+      'x-api-key': api_key
+    }})
+ .then((response) => {
+   return response.json();
+ })
+.then((data) => {
+   
+   //filter to only include those with an `image` object
+   data = data.filter(img=> img.image?.url!=null)
+   
+  storedBreeds = data;
+   
+   for (let i = 0; i < storedBreeds.length; i++) {
+    const breed = storedBreeds[i];
+    let option = document.createElement('option');
+     
+     //skip any breeds that don't have an image
+     if(!breed.image)continue
+     
+    //use the current array index
+    option.value = i;
+    option.innerHTML = `${breed.name}`;
+document.getElementById('breed_selector').appendChild(option);
+    
+    }
+   //show the first breed by default
+   showBreedImage(0)
+})
+.catch(function(error) {
+   console.log(error);
+});
+
+function showBreedImage(index)
+{ 
+  document.getElementById("breed_image").src= storedBreeds[index].image.url;
+  
+  document.getElementById("breed_json").textContent= storedBreeds[index].temperament
+  
+  
+  document.getElementById("wiki_link").href= storedBreeds[index].wikipedia_url
+  document.getElementById("wiki_link").innerHTML= storedBreeds[index].wikipedia_url
+}
